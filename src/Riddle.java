944737655d059d62e38682257a1da74d530e605e
IzzyR6 <league@WTS5.local>
2018-02-17 17:31:38 -0800

    Riddle

---
+++ src/Riddle.java
@@ -0,0 +1,21 @@
+import java.awt.Panel;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Riddle {
+	public static void main(String[] args) {
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JButton button = new JButton();
+JButton button2 = new JButton();
+JTextField textfield = new JTextField();
+JLabel label = new JLabel();
+frame.add(panel);
+panel.add(button);
+panel.add(button2);
+	}
+}
